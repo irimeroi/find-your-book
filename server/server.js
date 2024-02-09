7991b29be236062cce389a1af836f8514244873d
Irina Meroi <irimeroi7@gmail.com>
2024-02-09 02:47:55 -0300

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -26,11 +26,6 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
